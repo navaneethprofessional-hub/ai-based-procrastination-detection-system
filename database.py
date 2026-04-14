4b1ac1a0a2fdc6474608440f66498329938eeac3
Navaneeth S <navaneethprofessional@gmail.com>
2026-04-14 13:09:46 +0530

    Updated database.py with proper comments

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
-# python database.py
-
 import sqlite3
 
+# connect to database
 conn = sqlite3.connect("activity.db")
 cursor = conn.cursor()
 
+# fetch all records
 cursor.execute("SELECT * FROM activity_logs")
-
 rows = cursor.fetchall()
 
+# print data
 for row in rows:
     print(row)
 
+# close connection
 conn.close()
