ef4d5058ef565bcf93744d5d8ccca1c3a9832eb4
Navaneeth S <navaneethprofessional@gmail.com>
2026-04-21 08:53:09 +0530

    modified with proper comments and indentation

--- view_db.py
+++ view_db.py
@@ -1,13 +1,16 @@
 import sqlite3
 
+# connect to database
 conn = sqlite3.connect("activity.db")
 cursor = conn.cursor()
 
+# fetch all records
 cursor.execute("SELECT * FROM activity_logs")
-
 rows = cursor.fetchall()
 
+# display data
 for row in rows:
     print(row)
 
+# close connection
 conn.close()
